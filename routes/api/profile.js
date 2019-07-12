e5bdd74e61ffb3902aacc786dfe06bbbfd5241cd
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-07-12 01:30:00 -0700

    create auth tokens and profiles for users

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const auth = require("../../middleware/auth")
+const User = require('../../models/User')
+const Profile = require("../../models/Profile")
 
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('profile route'))
+// @route GET api/profile/me
+// @desc get concurrent user profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ["name", 'avatar'])
+
+        if (!profile) {
+            res.status(400).json({ msg: "no profile found"})
+        }
+
+        res.json(profile)
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('server error')
+    }
+})
 
 module.exports = router
