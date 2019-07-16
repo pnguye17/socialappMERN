c360ca289facad777e8f5383350bb534134c02e6
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-07-15 20:06:05 -0700

    delete exp from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -216,6 +216,30 @@ router.put(
        }
 )
 
+// @route DELETE api/profile/experience/EXP_ID
+// @desc  DELETE EXP FROM PROFILE
+// @access Private
+
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        })
+
+        //  remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+
+        profile.experience.splice(removeIndex, 1)
+        await profile.save()
+        res.json(profile)
+    } catch (error) {
+        console.error(err.message)
+        res.status(500).send("sever error")   
+    }
+})
+
+
 
 
 
