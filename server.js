b2f30ee51d95b1014b31208b9f99d7038fc17720
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-10 21:05:52 -0700

    building

--- server.js
+++ server.js
@@ -35,3 +35,5 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>
     console.log(`server started on port ${PORT}`))
+
+//please build website heroku!!!
