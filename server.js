5d3631654072b6e0a9cee698c40c84eb70a103a6
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-10 20:29:49 -0700

    prepare for deployment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+const path = require('path')
 
 // Connect Database
-// connectDB()
-require('./config/db')
+connectDB()
+// require('./config/db')
 
-app.get('/', (req, res) => res.send("API is running"))
 
 //init middlewar
 app.use(express.json({extended: false}))
@@ -18,6 +18,19 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/post', require('./routes/api/post'))
 
+
+
+//serve static assest  in prodcution 
+
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(
+        'client/build'
+    ))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>
